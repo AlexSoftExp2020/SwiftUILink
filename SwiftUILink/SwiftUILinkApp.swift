a299a33a72cec2b5f8506da0d134a032508c63c2
Alex <oleksii.pidhornyi@gmail.com>
2024-10-21 23:49:01 +0200

    Initial Commit

---
+++ SwiftUILink/SwiftUILinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILinkApp.swift
+//  SwiftUILink
+//
+//  Created by Oleksii on 21.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
