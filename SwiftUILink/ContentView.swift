bf5f2419051a8cae1cc506471bc248d441c61975
Alex <oleksii.pidhornyi@gmail.com>
2024-10-22 11:26:42 +0200

    add links

--- SwiftUILink/ContentView.swift
+++ SwiftUILink/ContentView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            LinkGoToApple()
+            LinkCallToAction()
+            LinkSendAnEmail()
+            LinkCredoAcademy()
+            CustomLink()
+            
         }
         .padding()
     }
@@ -21,4 +23,61 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct LinkGoToApple: View {
+    var body: some View {
+        Link("Go to Apple",
+             destination: URL(string: "https://apple.com")!
+        ).buttonStyle(.borderless)
+    }
+}
+
+struct LinkCallToAction: View {
+    var body: some View {
+        Link("Call To Action",
+             destination: URL(string: "tel:1234567890")!)
+        .buttonStyle(.bordered)
+    }
+}
+
+struct LinkSendAnEmail: View {
+    var body: some View {
+        Link("Send an Email",
+             destination: URL(string: "mailto: swiftui@apple.com")!)
+        .buttonStyle(.borderedProminent)
+        .buttonBorderShape(.capsule)
+        .controlSize(.large)
+        .tint(.pink)
+    }
+}
+
+struct LinkCredoAcademy: View {
+    var body: some View {
+        Link("Credo Academy",
+             destination: URL(string: "https://credo.academy")!)
+        .buttonStyle(.plain)
+        .padding()
+        .border(.primary, width: 2)
+    }
+}
+
+struct CustomLink: View {
+    var body: some View {
+        Link(destination: URL(string: "https://www.apple.com")!,
+             label: {
+            HStack(spacing: 16, content: {
+                Image(systemName: "apple.logo")
+                Text("Apple Store")
+            })
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .padding(.horizontal)
+            .background(
+                Capsule()
+                    .fill(Color.blue)
+            )
+        })
+    }
 }
